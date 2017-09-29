2d6a0a191e627d7af424145eefb1185735a40191
Machmud Zuhdi <machmud.zuhdialmunir@gmail.com>
2017-09-28 17:08:49 -0700

    LEVEL 1 - Validating the Form Values

--- form.php
+++ form.php
@@ -1,13 +1,31 @@
 <?php
-	if(isset($_POST['submit']))
+if(isset($_POST['submit']))
+{
+	$name = array("alexwawo", "machmud", "musa", "morata");
+	$min = 5;
+	$max = 10;
+	
+	$username = $_POST['username'];
+	$password = $_POST['password'];
+	
+	if(strlen($username)<$min)
 	{
-		$username = $_POST['username'];
-		$password = $_POST['password'];
-
-		echo $username;
-		echo "<br />";
-		echo $password;
+		echo "username harus memiliki panjang 5 atau lebih";
 	}
+	if(strlen($username)>$max)
+	{
+		echo "username tidak boleh lebih panjang dari 10";
+	}
+	
+	if(!in_array($username, $name))
+	{
+		echo "maaf, akses anda gagal";
+	}
+	else
+	{
+		echo "selamat datang bous!!";
+	}
+}
 ?>
 <!DOCTYPE>
 <html>
